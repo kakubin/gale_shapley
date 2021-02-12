48bf9d561ec07af9e07a59a807cfc79e54a03442
mrbigass <wetsand.wfs@gmail.com>
2021-02-12 18:58:31 +0900

    class化

--- lib/gale_shapley.rb
+++ lib/gale_shapley.rb
@@ -1,40 +1,42 @@
 require 'gale_shapley/version'
 
-def marriage(girls_ranks, boys_ranks)
-  pairs = {} # girlがキー
-  positions = {} # boyがキー
+class GaleShapley
+  def self.marriage(girls_ranks, boys_ranks)
+    pairs = {} # girlがキー
+    positions = {} # boyがキー
 
-  while true
-    bachelors = boys_ranks.keys - pairs.values
-    break if bachelors.empty?
+    while true
+      bachelors = boys_ranks.keys - pairs.values
+      break if bachelors.empty?
 
-    bachelors.each do |proposing_boy|
-      # 現在の順位を取得
-      position = positions[proposing_boy] ||= 0
-      girl = boys_ranks[proposing_boy][position]
+      bachelors.each do |proposing_boy|
+        # 現在の順位を取得
+        position = positions[proposing_boy] ||= 0
+        girl = boys_ranks[proposing_boy][position]
 
-      # 既に相手がいる場合
-      if pairs.has_key?(girl)
-        # 自分と敵対者の順位を調べて比較する
-        girls_rank = girls_ranks[girl.to_s.to_sym]
-        my_rank = girls_rank.index(proposing_boy.to_s)
-        opponent_rank = girls_rank.index(pairs[girl].to_s)
+        # 既に相手がいる場合
+        if pairs.has_key?(girl)
+          # 自分と敵対者の順位を調べて比較する
+          girls_rank = girls_ranks[girl.to_s.to_sym]
+          my_rank = girls_rank.index(proposing_boy.to_s)
+          opponent_rank = girls_rank.index(pairs[girl].to_s)
 
-        if my_rank < opponent_rank
-          # pairの入れ替え
+          if my_rank < opponent_rank
+            # pairの入れ替え
+            pairs[girl] = proposing_boy
+          end
+
+        else # 競合しなかった場合
           pairs[girl] = proposing_boy
         end
 
-      else # 競合しなかった場合
-        pairs[girl] = proposing_boy
+        positions[proposing_boy] += 1
       end
-
-      positions[proposing_boy] += 1
     end
-  end
 
-  # pairs.map { { _1 => _2.to_s } }
-  pairs
+    # pairs.map { { _1 => _2.to_s } }
+    pairs
+  end
 end
 
 girls_ranks = {
@@ -51,7 +53,7 @@ boys_ranks = {
   'd': ['1', '4', '3', '2'],
 }
 
-results = marriage(girls_ranks, boys_ranks)
+results = GaleShapley.marriage(girls_ranks, boys_ranks)
 
 puts 'falte' if results.nil?
 puts 'falte' unless results.length == 4
