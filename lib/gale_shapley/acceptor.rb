4812d25c34b15b908a4cc9b55e10d9f7fa8c2f11
kakubin <wetsand.wfs@gmail.com>
2022-12-18 21:30:34 +0900

    namespace

--- lib/gale_shapley/acceptor.rb
+++ lib/gale_shapley/acceptor.rb
@@ -1,25 +1,27 @@
 # frozen_string_literal: true
 
-class Acceptor # :nodoc:
-  attr_accessor :name, :proposer
+class GaleShapley # :nodoc:
+  class Acceptor # :nodoc:
+    attr_accessor :name, :proposer
 
-  def initialize(name, preferences)
-    @name = name
-    @preferences = preferences
-    @proposer = nil
-  end
+    def initialize(name, preferences)
+      @name = name
+      @preferences = preferences
+      @proposer = nil
+    end
 
-  def engaged?
-    !!@proposer
-  end
+    def engaged?
+      !!@proposer
+    end
 
-  def engage(proposer)
-    @proposer&.free
-    @proposer = proposer
-    @proposer.engage
-  end
+    def engage(proposer)
+      @proposer&.free
+      @proposer = proposer
+      @proposer.engage
+    end
 
-  def more_attractive?(proposer)
-    @preferences.index(proposer.name) < @preferences.index(@proposer.name)
+    def more_attractive?(proposer)
+      @preferences.index(proposer.name) < @preferences.index(@proposer.name)
+    end
   end
 end
--- lib/gale_shapley/proposer.rb
+++ lib/gale_shapley/proposer.rb
@@ -1,37 +1,39 @@
 # frozen_string_literal: true
 
-class Proposer # :nodoc:
-  attr_reader :name
+class GaleShapley # :nodoc:
+  class Proposer # :nodoc:
+    attr_reader :name
 
-  def initialize(name, preferences)
-    @name = name
-    @preferences = preferences
-    @index = 0
-    @engaged = false
-  end
-
-  def unengaged?
-    !@engaged
-  end
-
-  def engage
-    @engaged = true
-  end
-
-  def current_acceptor_name
-    @preferences[@index]
-  end
-
-  def propose(acceptor)
-    if acceptor.engaged?
-      acceptor.more_attractive?(self) ? acceptor.engage(self) : free
-    else
-      acceptor.engage(self)
+    def initialize(name, preferences)
+      @name = name
+      @preferences = preferences
+      @index = 0
+      @engaged = false
     end
-  end
 
-  def free
-    @index += 1
-    @engaged = false
+    def unengaged?
+      !@engaged
+    end
+
+    def engage
+      @engaged = true
+    end
+
+    def current_acceptor_name
+      @preferences[@index]
+    end
+
+    def propose(acceptor)
+      if acceptor.engaged?
+        acceptor.more_attractive?(self) ? acceptor.engage(self) : free
+      else
+        acceptor.engage(self)
+      end
+    end
+
+    def free
+      @index += 1
+      @engaged = false
+    end
   end
 end
